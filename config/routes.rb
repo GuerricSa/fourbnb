525f7ce25da339d7245fb7451382d2c5957b71d9
Cyrielle Marchand <cyrielle.marchand@gmail.com>
2023-08-28 16:55:52 +0200

    oven controller with index show new create edit update destroy

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
   root to: "ovens#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  resources :oven
+  resources :ovens
 end

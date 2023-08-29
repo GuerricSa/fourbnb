f7ef417fde743308f9c0aad137431bdfe69ea0e4
Cyrielle Marchand <cyrielle.marchand@gmail.com>
2023-08-29 11:28:29 +0200

    added seeds with faker gem

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,37 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+require 'faker'
+
+puts "Destroying all users and ovens"
+Oven.destroy_all
+User.destroy_all
+puts "Done!"
+
+puts "Creating users"
+5.times do
+  user = User.new(
+    email: Faker::Internet.email,
+    first_name: Faker::Name.first_name,
+    password: "123456"
+  )
+  user.save!
+end
+puts "Completed!"
+
+puts "Creating ovens"
+User.all.each do |user|
+  number_of_times = (0..3).to_a.sample
+  number_of_times.times do
+    Oven.create(
+      brand: Faker::Appliance.brand,
+      user_id: user.id,
+      address: Faker::Address.full_address,
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+      title: Faker::Commerce.product_name,
+      price: (1..50).to_a.sample
+    )
+  end
+end
+puts "Finished!"

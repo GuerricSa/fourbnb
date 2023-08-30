7dce8cc458bf6797c3c16d17a4f996f459579517
guerric sant <guerric.sant@gmail.com>
2023-08-30 14:28:53 +0200

    modify show (booking new) + modify css show&index

--- app/controllers/ovens_controller.rb
+++ app/controllers/ovens_controller.rb
@@ -7,6 +7,7 @@ class OvensController < ApplicationController
   end
 
   def show
+    @booking = Booking.new
   end
 
   def new
---
+++ app/javascript/controllers/toggle_controller.js
@@ -0,0 +1,14 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="toggle"
+export default class extends Controller {
+  static targets = ["togglableElement", "togglableElementForm"]
+  connect() {
+    console.log("Hello from toggle_controller")
+  }
+  move() {
+    console.log(this.togglableElementTarget)
+    this.togglableElementTarget.classList.toggle("d-none")
+    this.togglableElementFormTarget.classList.toggle("d-none")
+  }
+}

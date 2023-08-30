d885d16ba986e207dc83b0ba64ea11d91776dfa9
Cyrielle Marchand <cyrielle.marchand@gmail.com>
2023-08-30 16:24:49 +0200

    seed and navbar

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,50 +9,54 @@
 require 'faker'
 require "open-uri"
 
+CITIES = %w[Paris Lyon Blois Rennes Lille Marseille]
+
 puts "Destroying all users and ovens..."
 Oven.destroy_all
 User.destroy_all
 puts "Done!"
 
 puts "Creating users..."
-3.times do
+25.times do
   user = User.new(
     email: Faker::Internet.email,
     first_name: Faker::Name.first_name,
     password: "123456"
   )
-  file = URI.open(Faker::Avatar.image)
+  file = URI.open("https://source.unsplash.com/random/?avatar,face")
   user.avatar.attach(io: file, filename: "avatar#{user.id}.png", content_type: "image/png")
   user.save
 end
 puts "Completed!"
 
-puts "Creating user 'truc'..."
+puts "Creating ovens..."
+User.all.each do |user|
+  (1..3).to_a.sample.times do
+    oven = Oven.new(
+      brand: Faker::Appliance.brand,
+      user_id: user.id,
+      address: "#{Faker::Address.street_address}, #{CITIES.sample}",
+      description: Faker::Lorem.sentence(word_count: (50..80).to_a.sample),
+      title: "#{Faker::Adjective.positive} #{["four", "grill"].sample}",
+      price: (5..30).to_a.sample
+    )
+    [1, 5].sample.times do
+      # index = (0..8).to_a.sample
+      file = URI.open("https://source.unsplash.com/random/?oven")
+      oven.photos.attach(io: file, filename: "photo#{oven.id}.png", content_type: "image/png")
+      oven.save
+    end
+  end
+end
+puts "Finished!"
+
+puts "Creating 'Maité'..."
 truc = User.new(
-  email: "truc@gmail.com",
+  email: "maite@gmail.com",
   first_name: "Maité",
   password: "123456"
 )
 truc_avatar = URI.open("https://www.parismatch.com/lmnr/var/pm/public/media/image/2022/03/02/07/La-Cuisine-des-Mousquetaires-Que-devient-la-cuisiniere-Maite.jpg?VersionId=Bx1BN4WaCcUbOhuMd3mq5Y9ijidHeqJm")
 truc.avatar.attach(io: truc_avatar, filename: "maite.png", content_type: "image/png")
 truc.save
-puts "'truc' exists!"
-
-puts "Creating ovens..."
-User.all.each do |user|
-  number_of_times = (1..5).to_a.sample
-  number_of_times.times do
-    oven = Oven.new(
-      brand: Faker::Appliance.brand,
-      user_id: user.id,
-      address: Faker::Address.full_address,
-      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-      title: Faker::Commerce.product_name,
-      price: (1..50).to_a.sample
-    )
-    file = URI.open("https://images.unsplash.com/photo-1575219799974-8c4264b905f8?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80")
-    oven.photos.attach(io: file, filename: "photo#{oven.id}.png", content_type: "image/png")
-    oven.save
-  end
-end
-puts "Finished!"
+puts "'Maité' is alive!"

07751d8c9c69858a4a76352b929d3127fc00884e
kilian le doeuff <kilian78000@yahoo.fr>
2023-09-01 15:30:18 +0200

    modified review

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_31_141548) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_01_124802) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -71,7 +71,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_31_141548) do
 
   create_table "reviews", force: :cascade do |t|
     t.integer "rating"
-    t.string "content"
+    t.text "content"
     t.bigint "booking_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
